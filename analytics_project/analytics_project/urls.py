fc58d5738405ac8747f9cc3ba6e21adc9d5c51a0
Vaibob <vaibhavshelar269@gmail.com>
2022-09-30 02:20:36 +0530

    Visualise equities as line graph

--- analytics_project/analytics_project/urls.py
+++ analytics_project/analytics_project/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from dashboard import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('dashboard/', include('dashboard.urls')),
+    path('api', views.ChartData.as_view()),
+    path('', views.home)
 ]
--- analytics_project/dashboard/views.py
+++ analytics_project/dashboard/views.py
@@ -2,13 +2,51 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from dashboard.models import Order
 from django.core import serializers
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-def dashboard_with_pivot(request):
-    return render(request, 'dashboard_with_pivot.html', {})
+def chart(request):
+    return render(request, 'chart.html', {})
 
 
-def pivot_data(request):
-    dataset = Order.objects.all()
-    data = serializers.serialize('json', dataset)
-    return JsonResponse(data, safe=False)
+class ChartData(APIView):
+    authentication_classes = []
+    permission_classes = []
+   
+    def get(self, request, format = None):
+        x_ = Order.objects.filter(equity_id=request.session['equity_id']).order_by('date').values()
+        labels, chartdata = [], []
+        for row in x_:
+            labels.append(row['date'])
+            chartdata.append(row['returns'])
+        chartLabel = "Equity return"
+        data ={
+                     "labels":labels,
+                     "chartLabel":chartLabel,
+                     "chartdata":chartdata,
+             }
+        return Response(data)
 
+def home(request):
+    if request.method == 'GET':
+        return render(request, 'index.html')
+    else:
+        request.session['equity_id'] = request.POST['inp']
+
+        equities = {
+            '10277':'Agilent Technologies Inc',
+            '10278':'Alcoa Inc', 
+            '10279':'Yahoo! Inc' ,
+            '10280':'American Addiction Centers',
+            '10281':'American Airlines Group Inc', 
+            '10282':'Altisource Asset Management Corp',
+            '10283':'Atlantic American Corp', 
+            '10284':"Aaron's, Inc",
+            '10285':'Applied Optoelectronics Inc',
+            '10286':"AAON, Inc" 
+        }
+
+        return render(request, 'chart.html', context={
+            'equity_id': request.session['equity_id'],
+            'equity_name': equities[request.session['equity_id']]
+        })
